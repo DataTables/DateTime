a493c40ee2095192f69fec8f5708e5e4f1b82d72
Allan Jardine <allan@datatables.net>
2021-11-17 09:34:28 +0000

    Update to build tools

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
  
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 
 gulp.task('scss', function() {
 	return gulp.src('./css/dataTables.dateTime.scss')
