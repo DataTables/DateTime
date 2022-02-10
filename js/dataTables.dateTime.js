3aa485eeb134745de4097894b65ca3c79d99c969
Allan Jardine <allan@datatables.net>
2022-02-10 15:56:38 +0000

    Release 1.1.2

--- js/dataTables.dateTime.js
+++ js/dataTables.dateTime.js
@@ -1,4 +1,4 @@
-/*! DateTime picker for DataTables.net v1.1.1
+/*! DateTime picker for DataTables.net v1.1.2
  *
  * © SpryMedia Ltd, all rights reserved.
  * License: MIT datatables.net/license/mit
@@ -6,7 +6,7 @@
 
 /**
  * @summary     DateTime picker for DataTables.net
- * @version     1.1.1
+ * @version     1.1.2
  * @file        dataTables.dateTime.js
  * @author      SpryMedia Ltd
  * @contact     www.datatables.net/contact
@@ -1535,7 +1535,7 @@ DateTime.defaults = {
 	yearRange: 25
 };
 
-DateTime.version = '1.1.1';
+DateTime.version = '1.1.2';
 
 // Global export - if no conflicts
 if (! window.DateTime) {
