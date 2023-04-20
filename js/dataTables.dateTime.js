2a65be165a6f4b5601dc8a0d57fed231426e5fd5
Allan Jardine <allan@datatables.net>
2023-04-20 09:47:47 +0100

    1.4.1 release

--- js/dataTables.dateTime.js
+++ js/dataTables.dateTime.js
@@ -1,4 +1,4 @@
-/*! DateTime picker for DataTables.net v1.4.0
+/*! DateTime picker for DataTables.net v1.4.1
  *
  * © SpryMedia Ltd, all rights reserved.
  * License: MIT datatables.net/license/mit
@@ -6,7 +6,7 @@
 
 /**
  * @summary     DateTime picker for DataTables.net
- * @version     1.4.0
+ * @version     1.4.1
  * @file        dataTables.dateTime.js
  * @author      SpryMedia Ltd
  * @contact     www.datatables.net/contact
@@ -1613,7 +1613,7 @@ DateTime.defaults = {
 	yearRange: 25
 };
 
-DateTime.version = '1.4.0';
+DateTime.version = '1.4.1';
 
 /**
  * CommonJS factory function pass through. Matches DataTables.
