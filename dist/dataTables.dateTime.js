7bda40522aba79dbc24bd0e06c545133eaeb21dd
Allan Jardine <allan@datatables.net>
2024-07-24 11:35:22 +0000

    Release 1.5.3

--- dist/dataTables.dateTime.js
+++ dist/dataTables.dateTime.js
@@ -1,4 +1,4 @@
-/*! DateTime picker for DataTables.net v1.5.2
+/*! DateTime picker for DataTables.net v1.5.3
  *
  * © SpryMedia Ltd, all rights reserved.
  * License: MIT datatables.net/license/mit
@@ -48,7 +48,7 @@
 
 /**
  * @summary     DateTime picker for DataTables.net
- * @version     1.5.2
+ * @version     1.5.3
  * @file        dataTables.dateTime.js
  * @author      SpryMedia Ltd
  * @contact     www.datatables.net/contact
@@ -1690,7 +1690,7 @@ DateTime.defaults = {
 	yearRange: 25
 };
 
-DateTime.version = '1.5.2';
+DateTime.version = '1.5.3';
 
 /**
  * CommonJS factory function pass through. Matches DataTables.
--- dist/dataTables.dateTime.mjs
+++ dist/dataTables.dateTime.mjs
@@ -1,4 +1,4 @@
-/*! DateTime picker for DataTables.net v1.5.2
+/*! DateTime picker for DataTables.net v1.5.3
  *
  * © SpryMedia Ltd, all rights reserved.
  * License: MIT datatables.net/license/mit
@@ -12,7 +12,7 @@ let $ = jQuery;
 
 /**
  * @summary     DateTime picker for DataTables.net
- * @version     1.5.2
+ * @version     1.5.3
  * @file        dataTables.dateTime.js
  * @author      SpryMedia Ltd
  * @contact     www.datatables.net/contact
@@ -1654,7 +1654,7 @@ DateTime.defaults = {
 	yearRange: 25
 };
 
-DateTime.version = '1.5.2';
+DateTime.version = '1.5.3';
 
 /**
  * CommonJS factory function pass through. Matches DataTables.
